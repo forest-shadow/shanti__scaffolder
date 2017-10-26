b43ab12f43a33d8e7ca2e325db69f4b9d4f17dd7
forest-shadow <mamasay@yandex.ru>
2017-10-26 13:12:46 +0300

    remove gulp update tasks; resolve bitters font var conflict; remove bourbon & neat from vendor directory (use direct path from node_modules now); add *.log files to .gitignore; update bourbon dependency to v5 beta

--- gulp/tasks/update/update.normalize.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/*******************************
- * Task: Update normalize.scss
- *******************************/
-$.gulp.task( 'updateNormalize', function() {
-  return $.gulp.src( './bower_components/normalize-scss/_normalize.scss' )
-      .pipe( $.gulp.dest( $.config.paths.scssVendor + '/' ) )
-});
